8ba7cb8e609420993e4b3cbc4b029e2e45551d8a
varshith03 <svarshith.vs@gmail.com>
2024-02-18 19:58:58 +0530

    added new background and mobile responsiveness

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pickColorBtn = document.getElementById('pickColorBtn');
-    const imageInput = document.getElementById('upload');
-    const imageContainer = document.getElementById('image-container');
-    const result = document.getElementById('result');
+document.addEventListener("DOMContentLoaded", () => {
+  const pickColorBtn = document.getElementById("pickColorBtn");
+  const imageInput = document.getElementById("upload");
+  const imageContainer = document.getElementById("image-container");
+  const result = document.getElementById("result");
 
-    pickColorBtn.addEventListener('click', colorPicker);
+  pickColorBtn.addEventListener("click", colorPicker);
 
-    function colorPicker() {
-        const eyeDropper = new EyeDropper();
-        eyeDropper.open().then(({ sRGBHex }) => {
-            result.innerHTML = sRGBHex;
-        });
+  function colorPicker() {
+    const eyeDropper = new EyeDropper();
+    eyeDropper.open().then(({ sRGBHex }) => {
+      result.innerHTML = "Color Value: " + sRGBHex;
+    });
+  }
+
+  imageInput.addEventListener("change", handleImageUpload);
+
+  function handleImageUpload() {
+    const file = imageInput.files[0];
+
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const img = new Image();
+        img.src = e.target.result;
+        // Display the image in the container
+        imageContainer.innerHTML = "";
+        imageContainer.appendChild(img);
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      alert("Please choose a valid image file.");
+      imageInput.value = "";
     }
-
-    imageInput.addEventListener('change', handleImageUpload);
-
-    function handleImageUpload() {
-        const file = imageInput.files[0];
-
-        if (file && file.type.startsWith('image/')) {
-            const reader = new FileReader();
-
-            reader.onload = function (e) {
-                const img = new Image();
-                img.src = e.target.result;
-                img.style.width = '800px';
-                img.style.height = '400px';
-                img.style.objectFit = 'cover';
-
-                // Display the image in the container
-                imageContainer.innerHTML = '';
-                imageContainer.appendChild(img);
-            };
-
-            reader.readAsDataURL(file);
-        } else {
-            alert('Please choose a valid image file.');
-            // Clear the input field
-            imageInput.value = '';
-        }
-    }
+  }
 });
